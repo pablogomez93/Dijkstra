7f78b5b639802105911a7209005d4e1975ecb15a
Pablo Gómez <mago-1986@hotmail.com>
2015-07-11 21:54:13 -0300

    Deleted unnecessary 'this->' sentence. Gracias Fede

--- graph.cpp
+++ graph.cpp
@@ -1,45 +1,45 @@
 #include "graph.h"
 
 Graph::Graph(uint n, bool isOrientedGraph, IMPL impl){
-	this->n = n;
-	this->m = 0;
-	this->isOriented = isOrientedGraph;
-	this->type = impl;
-	this->edges = vector<Edge>();
-	if(this->type == ADJACENCIES_MATRIX)
-		this->matrix = vector<vector<float> > (n, vector<float>(n, DEFAULT_WEIGHT));
+	n = n;
+	m = 0;
+	isOriented = isOrientedGraph;
+	type = impl;
+	edges = vector<Edge>();
+	if(type == ADJACENCIES_MATRIX)
+		matrix = vector<vector<float> > (n, vector<float>(n, DEFAULT_WEIGHT));
 	else
-		this->adjList = vector<list<pair<uint,float> > >(n, list<pair<uint,float> >());
+		adjList = vector<list<pair<uint,float> > >(n, list<pair<uint,float> >());
 }
 
 bool Graph::isOrientedGraph() const{
-	return this->isOriented;
+	return isOriented;
 }
 
 uint Graph::getN() const{
-	return this->n;
+	return n;
 }
 
 uint Graph::getM() const{
-	return this->m;
+	return m;
 }
 
 vector<Edge>& Graph::getEdges(){
-	return this->edges;
+	return edges;
 }
 
 float Graph::getEdgeWeight(uint v1, uint v2) const{
-	if(this->type == ADJACENCIES_MATRIX)
-		return this->matrix[v1-1][v2-1];
-	else 
+	if(type == ADJACENCIES_MATRIX)
+		return matrix[v1-1][v2-1];
+	else
 		for (list<pair<uint,float> >::const_iterator it = adjList[v1-1].begin(); it != adjList[v1-1].end(); it++)
 			if((*it).first == v2)
 				return (*it).second;
 }
 
 bool Graph::areAdjacent(uint v1, uint v2) const{
-	if(this->type == ADJACENCIES_MATRIX)
-		return this->matrix[v1-1][v2-1] != DEFAULT_WEIGHT;
+	if(type == ADJACENCIES_MATRIX)
+		return matrix[v1-1][v2-1] != DEFAULT_WEIGHT;
 	else{
 		for (list<pair<uint,float> >::const_iterator it = adjList[v1-1].begin(); it != adjList[v1-1].end(); it++)
 			if((*it).first == v2)
@@ -50,69 +50,69 @@ bool Graph::areAdjacent(uint v1, uint v2) const{
 }
 
 void Graph::applyEdge(uint v1, uint v2){
-	if(this->type == ADJACENCIES_MATRIX){
-		if(this->matrix[v1-1][v2-1] == DEFAULT_WEIGHT){
-			this->m++;
-			this->edges.push_back(Edge(v1,v2,1));
+	if(type == ADJACENCIES_MATRIX){
+		if(matrix[v1-1][v2-1] == DEFAULT_WEIGHT){
+			m++;
+			edges.push_back(Edge(v1,v2,1));
 		}
-		
-		this->matrix[v1-1][v2-1] = 1;
 
-		if(!this->isOriented)
-			this->matrix[v2-1][v1-1] = 1;
+		matrix[v1-1][v2-1] = 1;
+
+		if(!isOriented)
+			matrix[v2-1][v1-1] = 1;
 	}else{
 		adjList[v1-1].push_back(make_pair(v2,1));
-	
-		if(!this->isOriented)
+
+		if(!isOriented)
 			adjList[v2-1].push_back(make_pair(v1,1));
 
-		this->m++;
-		this->edges.push_back(Edge(v1,v2,1));
+		m++;
+		edges.push_back(Edge(v1,v2,1));
 	}
 }
 
 void Graph::applyEdge(uint v1, uint v2, float weight){
-	if(this->type == ADJACENCIES_MATRIX){
-		if(this->matrix[v1-1][v2-1] == DEFAULT_WEIGHT){
-			this->m++;
-			this->edges.push_back(Edge(v1,v2,weight));
+	if(type == ADJACENCIES_MATRIX){
+		if(matrix[v1-1][v2-1] == DEFAULT_WEIGHT){
+			m++;
+			edges.push_back(Edge(v1,v2,weight));
 		}
-		
-		this->matrix[v1-1][v2-1] = weight;
 
-		if(!this->isOriented)
-			this->matrix[v2-1][v1-1] = weight;
+		matrix[v1-1][v2-1] = weight;
+
+		if(!isOriented)
+			matrix[v2-1][v1-1] = weight;
 	}else{
 		adjList[v1-1].push_back(make_pair(v2, weight));
-	
-		if(!this->isOriented)
-			adjList[v2-1].push_back(make_pair(v1, weight));		
 
-		this->m++;
-		this->edges.push_back(Edge(v1,v2,weight));
+		if(!isOriented)
+			adjList[v2-1].push_back(make_pair(v1, weight));
+
+		m++;
+		edges.push_back(Edge(v1,v2,weight));
 	}
 
 }
 
 void Graph::addVertex(){
-	if(this->type == ADJACENCIES_MATRIX){
-		for (int i = 0; i < this->n; ++i)
-			this->matrix[i].push_back(DEFAULT_WEIGHT);
+	if(type == ADJACENCIES_MATRIX){
+		for (int i = 0; i < n; ++i)
+			matrix[i].push_back(DEFAULT_WEIGHT);
 
-		this->matrix.push_back(vector<float>(this->n + 1, DEFAULT_WEIGHT));
-	
+		matrix.push_back(vector<float>(n + 1, DEFAULT_WEIGHT));
+
 	}else{
-		this->adjList.push_back(list<pair<uint,float> >());
+		adjList.push_back(list<pair<uint,float> >());
 	}
 
-	this->n++;
+	n++;
 }
 
 const typename Graph::Iterator Graph::adjacentsOf(uint v) const{
-	if(this->type == ADJACENCIES_MATRIX)
-		return Iterator(this->matrix[v-1], this->n, this->type);
+	if(type == ADJACENCIES_MATRIX)
+		return Iterator(matrix[v-1], n, type);
 	else
-		return Iterator(this->adjList[v-1], this->n, this->type);
+		return Iterator(adjList[v-1], n, type);
 }
 
 
@@ -133,7 +133,7 @@ Graph::Iterator::Iterator(const vector<float>& conections, uint n, IMPL type){
 			_current = current;
 			break;
 		}
-	
+
 	_vSpace = n;
 	_type = type;
 }
@@ -141,7 +141,7 @@ Graph::Iterator::Iterator(const vector<float>& conections, uint n, IMPL type){
 Graph::Iterator::Iterator(const list<pair<uint,float> >& adjacents, uint n, IMPL type){
  	_adjacents = adjacents;
 	_iter = _adjacents.begin();
-	
+
 	_vSpace = n;
 	_type = type;
 }
@@ -171,7 +171,7 @@ int Graph::Iterator::next() const{
 
 bool Graph::Iterator::thereIsMore() const{
 	if(_type == ADJACENCIES_MATRIX)
-		return -1 < _current && _current < _vSpace;	
+		return -1 < _current && _current < _vSpace;
 	else
 		return _iter != _adjacents.end();
 }
